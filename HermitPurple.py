cda476eec5ffa35aa76125530b58227d1fe3ae6b
Nicolas Barragan <nicolas.barragan@upb.edu.co>
2021-05-26 08:04:05 -0500

    Imprimir regiones de interes

--- HermitPurple.py
+++ HermitPurple.py
@@ -19,6 +19,12 @@ estado = 0
 #captura
 captura = cv2.VideoCapture(0, cv2.CAP_DSHOW) #acceso a la camara integrada del computador
 
+#ROI
+ROI_superior = [300,50,400,150]
+ROI_inferior = [300,250,400,350]
+ROI_izquierda = [200,150,300,250]
+ROI_derecha = [400,150,500,250]
+
 with mp_face_detection.FaceDetection(min_detection_confidence=0.8) as face_detection:
     
     while(True):
@@ -44,6 +50,13 @@ with mp_face_detection.FaceDetection(min_detection_confidence=0.8) as face_detec
             fotograma = cv2.flip(fotograma,1)
             fotograma_rgb = cv2.cvtColor(fotograma, cv2.COLOR_BGR2RGB)
             
+            #Imprimir las regiones de interes
+            cv2.rectangle(fotograma,(ROI_superior[0],ROI_superior[1]),(ROI_superior[2],ROI_superior[3]),(0,255,0),2)
+            cv2.rectangle(fotograma,(ROI_inferior[0],ROI_inferior[1]),(ROI_inferior[2],ROI_inferior[3]),(0,255,0),2)
+            cv2.rectangle(fotograma,(ROI_izquierda[0],ROI_izquierda[1]),(ROI_izquierda[2],ROI_izquierda[3]),(0,255,0),2)
+            cv2.rectangle(fotograma,(ROI_derecha[0],ROI_derecha[1]),(ROI_derecha[2],ROI_derecha[3]),(0,255,0),2)
+            
+            #Calcular los puntos de interes del rostro
             resultados = face_detection.process(fotograma_rgb)
             
             if resultados.detections is not None:
@@ -52,7 +65,7 @@ with mp_face_detection.FaceDetection(min_detection_confidence=0.8) as face_detec
                     xN = int(detection.location_data.relative_keypoints[2].x * width)
                     yN = int(detection.location_data.relative_keypoints[2].y * height)
                     #Dibujar un circulo en la punta de la nariz
-                    cv2.circle(fotograma,(xN,yN),5,(255,255,0), -1)
+                    cv2.circle(fotograma,(xN,yN),10,(255,255,0), -1)
             
             #Mostrar la captura
             cv2.imshow("Camara", fotograma)
